16b42c02c15c26ed2ef76060b5ca1e0437146a85
Graham Preston <graham.preston@gmail.com>
2023-12-15 13:41:59 +0100

    Solution except the library's function doesn't work 😡

--- day-11.go
+++ day-11.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-    "github.com/dominikbraun/graph"
-    "fmt"
-    "strings"
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/dominikbraun/graph"
+    "gonum.org/v1/gonum/stat/combin"
 )
 
 type pos struct {
@@ -11,59 +14,153 @@ type pos struct {
     y int
 }
 
+func (p pos) Less(comp pos) bool {
+    if p.x < comp.x {
+        return true
+    }
+
+    if p.y < comp.y {
+        return true
+    }
+
+    return false
+}
+
+type pair struct {
+    lowPos pos
+    highPos pos
+}
+
+func expandSpace(space []string) []string {
+    emptyRows := []int{}
+    emptyCols := []int{}
+
+    for row, line := range space {
+        if !strings.Contains(line, "#") {
+            emptyRows = append(emptyRows, row)
+        }
+    }
+
+    for col := 0; col < len(space[0]); col++ {
+        isEmpty := true
+
+        for _, line := range space {
+            if line[col] == '#' {
+                isEmpty = false
+            }
+        }
+
+        if isEmpty {
+            emptyCols = append(emptyCols, col)
+        }
+    }
+
+    var expandedSpace []string
+
+    for row, line := range space {
+        var expandedLineBuilder strings.Builder
+
+        for col := 0; col < len(line); col++ {
+            if slices.Contains(emptyCols, col) {
+                expandedLineBuilder.WriteString("..")
+            } else {
+                expandedLineBuilder.WriteByte(line[col])
+            }
+        }
+
+        expandedLine := expandedLineBuilder.String()
+        expandedSpace = append(expandedSpace, expandedLine)
+
+        if slices.Contains(emptyRows, row) {
+            expandedSpace = append(expandedSpace, expandedLine)
+        }
+    }
+
+    return expandedSpace
+}
+
 func RunDay11() {
     // Read input
-    contents := ReadContents("input/day-11.txt")
+    contents := ReadContents("input/test.txt")
     lines := strings.Split(contents, "\n")
 
     // Expand space
-    withExpandedRows := [][]rune{}
-
-    for _, line := range lines {
-        if !strings.Contains(line, "#") {
-            for i := 0; i <= 2; i++ {
-                withExpandedRows = append(withExpandedRows, []rune(line))
-            }
-        } else {
-            withExpandedRows = append(withExpandedRows, []rune(line))
-        }
-    }
-
-    withExpandedCols := [][]rune{}
-
-    for col := 0; col < len(withExpandedRows[0]); col++ {
-        colChars := Map(withExpandedRows, func(row []rune) rune { return row[col] })
-
-        if !Any(colChars, func(char rune) bool { return char == '#' }) {
-            for row
-        }
-    }
-
+    expandedSpace := expandSpace(lines)
 
     // Create graph
-
-    // Find paths
-
-    // Result
-
-    // -------
-
     posHash := func(p pos) pos {
         return p
     }
 
     g := graph.New(posHash)
+    galaxyPositions := []pos{}
 
-    g.AddVertex(pos{0, 0})
-    g.AddVertex(pos{1, 0})
-    g.AddVertex(pos{0, 1})
+    for row := 0; row < len(expandedSpace); row++ {
+        for col := 0; col < len(expandedSpace[0]); col++ {
+            currentPos := pos{row, col}
+            g.AddVertex(currentPos)
 
-    g.AddEdge(pos{0, 0}, pos{1, 0})
-    g.AddEdge(pos{0, 0}, pos{0, 1})
-    g.AddEdge(pos{0, 0}, pos{1, 0})
+            if expandedSpace[row][col] == '#' {
+                galaxyPositions = append(galaxyPositions, currentPos)
+            }
 
-    graph.DFS(g, pos{1, 0}, func(value pos) bool {
-        fmt.Println(value)
-        return false
-    })
+            if row > 0 {
+                g.AddEdge(pos{row - 1, col}, currentPos)
+            }
+
+            if col > 0 {
+                g.AddEdge(pos{row, col - 1}, currentPos)
+            }
+        }
+    }
+
+    // Find paths
+    // inefficient
+    /*
+    totalLen := 0
+
+    for _, c := range combin.Combinations(len(galaxyPositions), 2) {
+        start := galaxyPositions[c[0]]
+        target := galaxyPositions[c[1]]
+
+        path, err := graph.ShortestPath(g, start, target)
+        Check(err)
+        totalLen += len(path) - 1
+    }
+    */
+
+    // better
+    foundPaths := make(map[pair]bool)
+    totalCombos := len(combin.Combinations(len(galaxyPositions), 2))
+    totalLen := 0
+
+    for _, p := range galaxyPositions {
+        fmt.Println("cehcking galaxy", p)
+        if len(foundPaths) < totalCombos {
+            graph.BFSWithDepth(g, p, func(v pos, depth int) bool {
+                fmt.Println(p, v, depth)
+                if v != p && expandedSpace[v.x][v.y] == '#' {
+                    var thePair pair
+
+                    if p.Less(v) {
+                        thePair = pair{lowPos: p, highPos: v}
+                    } else {
+                        thePair = pair{lowPos: v, highPos: p}
+                    }
+
+                    if !foundPaths[thePair] {
+                        fmt.Println("found a path", p, v, depth)
+                        foundPaths[thePair] = true
+                        totalLen += depth
+                    }
+                }
+
+                return len(foundPaths) == totalCombos
+            })
+            return
+        }
+    }
+
+    // Result
+    fmt.Println("Day 11 part 1:", totalLen)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/gcpreston/aoc2023go
 go 1.21.5
 
 require github.com/dominikbraun/graph v0.23.0
+
+require gonum.org/v1/gonum v0.14.0 // indirect
